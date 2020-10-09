df40ddff333031f3a4839660b692ac2f24a9994e
ChimbiliSuprajaChowdary <supraja.cs1995@gmail.com>
2020-10-09 10:42:43 +0530

    done

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,29 @@
+package controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static void main(String args[]) throws ClassNotFoundException, SQLException
+	{
+		long id = 0;
+		String name = null;
+		List<Skill>  list= new ArrayList<Skill>();
+	//	Skill skill=new Skill(id,name);
+		SkillDAO skilldao=new SkillDAO();
+		
+		skilldao.listAllSkills();
+		System.out.println("List of all Skills");
+		for(Skill s: list)
+		{
+				System.out.println(s.getSkill_id()+" "+s.getSkill_name());
+		}
+		
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,33 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	static List<Skill> list=new ArrayList<Skill>();
+	
+	public List<Skill> listAllSkills () throws SQLException, ClassNotFoundException
+	{
+		 Connection con = ConnectionManager.getConnection();
+			Statement st = con.createStatement();
+
+			ResultSet rs = st.executeQuery("SELECT * FROM SKILL ORDER BY name");
+			while(rs.next())
+			{
+				long id=rs.getLong(1);
+				String name=rs.getString(2);
+				Skill skill=new Skill(id,name);
+				list.add(skill);
+			}
+
+			return list;
+	}
+}
